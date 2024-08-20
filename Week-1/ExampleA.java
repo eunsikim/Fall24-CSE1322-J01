f78d5c619649dcb05f6897c3d2963d80f1b37cdb
eunsikim <kimeunsik229@gmail.com>
2024-08-20 19:01:09 -0400

    Example for Week 1

---
+++ Week-1/ExampleA.java
@@ -0,0 +1,8 @@
+// Invert a char array
+public class ExampleA {
+    public static void main(String[] args) {
+        char[] array = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+
+        
+    }
+}
