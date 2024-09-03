e47c111d4820559f872d606112d6c9dfa8f35804
eunsikim <kimeunsik229@gmail.com>
2024-09-03 19:50:25 -0400

    Week 4 material

--- Week-3/ExampleD.java
+++ Week-3/ExampleD.java
@@ -1,17 +1,18 @@
 public class ExampleD {
     public static void main(String[] args) {
-        float f = 3.68823987325f; //368.123...
+        float f = 3.68823987325f;
 
+        // `number` does not get rounded
         int number = (int)f;
-
         System.out.println(number);
 
+        // Approach one using `Math.round()` <- Round to the next integer
         int number2 = Math.round(f);
-
         System.out.println(number2);
-
         float dollars = Math.round(f * 100) / 100f;
         System.out.println(dollars);
+
+        // Approach 2: Using format
         System.out.format("%.2f", f);
     }
 }
