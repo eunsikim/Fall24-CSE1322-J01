6338a6f57c2abf1c4115beee1b0269471ef486a6
Kim Eun Sik <kimeunsik229@gmail.com>
2024-10-22 20:26:08 -0400

    Update ExampleB.java

--- Week-10/ExampleB.java
+++ Week-10/ExampleB.java
@@ -1,17 +1,18 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class ExampleB {
     public static void main(String[] args) {
-        final String FILE_PATH = "./TODO.TXT";
+        final String FILE_PATH = "./TODO.TXT"; // Whichever the path to your file is.
 
-        try (BufferedWriter wr = new BufferedWriter(new FileWriter(FILE_PATH))) {
-
+        // try (BufferedWriter wr = new BufferedWriter(new FileWriter(FILE_PATH,  true))) { // append to text file
+        try (BufferedWriter wr = new BufferedWriter(new FileWriter(FILE_PATH))) { // Overwrite text file contents
             wr.write("4. Work on Assignment 6\n");
             wr.write("5. Work on Lab 6\n");
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            e.getMessage();
         }
 
     }
