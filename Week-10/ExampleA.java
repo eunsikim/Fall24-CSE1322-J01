5fd23f8a6930d35abcf537bc884563c7ce5a7f09
Kim Eun Sik <kimeunsik229@gmail.com>
2024-10-22 20:26:51 -0400

    Update ExampleA.java

--- Week-10/ExampleA.java
+++ Week-10/ExampleA.java
@@ -1,17 +1,18 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ExampleA {
     public static void main(String[] args) {
-        final String FILE_PATH = "./TODO.TXT";
+        final String FILE_PATH = "./TODO.TXT"; // Whichever the path to your file is.
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
 
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            e.getMessage();
         }
 
     }
